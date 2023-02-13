7c6f09e44e37fbb482e1265bc9867e1f1e5194da
Numan Dirican <numan_859@hotmail.com>
2023-02-13 16:27:32 +0300

    Initialize project using create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./style.css"
+import TextField from '@mui/material/TextField';
+
+function App() {
+  return (
+    <div>
+
+    <h1 className="page-title">Todo List App </h1>
+    <TextField id="outlined-basic" label="Outlined" variant="outlined"  />
+    </div>
+  );
+}
+
+export default App;
