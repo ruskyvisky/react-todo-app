7a7f49fbb2806a99f6d57162d309d66783f347f9
Numan Dirican <numan_859@hotmail.com>
2023-02-13 18:34:15 +0300

    create UI design with dummy data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import { useState } from "react";
-import "./style.css"
-import TextField from '@mui/material/TextField';
-
+import "./style.css";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
+import { ListItem, ListItemAvatar, ListItemText,List } from "@mui/material";
 function App() {
+  const dummyData=[
+    {id:1, title:"Todo 1", todoCategory:"House",    completed:false,isEditable:false},
+    {id:2, title:"Todo 2" ,todoCategory:"Work" , completed:true ,isEditable:false},
+    {id:3, title:"Todo 3",todoCategory:"House", completed:false ,isEditable:false},
+  ]
   return (
-    <div>
-
-    <h1 className="page-title">Todo List App </h1>
-    <TextField id="outlined-basic" label="Outlined" variant="outlined"  />
+    <div className="page">
+      <h1 className="page-title">Todo List App </h1>
+      <sub className="page-subtitle">Add your todos here</sub>
+     <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" ,  p: 2, }}> 
+     <TextField
+        className="input-field"
+        label="Todo"
+        variant="outlined"
+      />
+   
+     
+     </Box>
+     <Button variant="contained" className="add-button" >Add Todo</Button>
+    {
+      dummyData.map((todo)=>{
+        return  <List sx={{ width: '100%', maxWidth: 360, }}>
+        <ListItem>
+        <ListItemAvatar>
+      
+        </ListItemAvatar>
+        <ListItemText primary={todo.title} secondary={todo.todoCategory} />
+      </ListItem>
+      </List>
+      })
+    }
     </div>
   );
 }
